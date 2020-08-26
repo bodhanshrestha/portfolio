fdbf2f5d6ae0517095c8c48cb7c1fbc87c3ac60c
Bodhan Shrestha <bodhanshrestha@gmail.com>
2020-08-26 20:11:10 +0545

    Added mouse follower

--- src/components/Helper/scrollDown.js
+++ src/components/Helper/scrollDown.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons';
 const ToTop = () => {
+  const [show, setShow] = useState('1');
+  useEffect(() => {
+    const width = window.innerWidth;
+    if (width < 1024) {
+      setShow('0');
+    } else {
+      setShow('1');
+    }
+  }, []);
   return (
-    <div className='newBtn'>
+    <div className='newBtn' style={{ opacity: show }}>
       Scroll Down &nbsp; &nbsp;
       <FontAwesomeIcon icon={faLongArrowAltUp} />
     </div>
--- src/index.js
+++ src/index.js
@@ -1,24 +1,79 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 import ScrollTop from './components/Helper/scrollToTop';
 import Header from './components/Header/header';
 import Body from './components/routes';
 import './index.scss';
 
 function App() {
+  useEffect(() => {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        document.querySelector('.header').style.padding = '12px 40px';
+      } else {
+        document.querySelector('.header').style.padding = '15px 30px';
+      }
+
+      var height =
+        document.body.scrollTop || document.documentElement.scrollTop;
+
+      var totalHeight =
+        document.body.clientHeight || document.documentElement.clientHeight;
+
+      var totalHeight = totalHeight - 605;
+      var client = (height / totalHeight) * 100;
+      document.querySelector('.window-bar').style.width = `${client}%`;
+
+      if (
+        document.body.scrollTop > totalHeight - 100 ||
+        document.documentElement.scrollTop > totalHeight - 100
+      ) {
+        document.querySelector('.newBtn').style.display = 'none';
+      } else {
+        document.querySelector('.newBtn').style.display = 'block';
+      }
+    }
+
+    const curser = document.querySelector('.curser');
+
+    document.addEventListener('mousemove', (e) => {
+      curser.setAttribute(
+        'style',
+        'top:' +
+          (e.pageY - 20) +
+          'px;left:' +
+          (e.pageX + 20) +
+          'px; transition:60ms;'
+      );
+    });
+    document.addEventListener('click', () => {
+      curser.classList.add('expand');
+      setTimeout(() => {
+        curser.classList.remove('expand');
+      }, 500);
+    });
+  }, []);
   return (
     <div className='App'>
       <Header />
       <Body />
+      <div className='curser'></div>
     </div>
   );
 }
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <ScrollTop />
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
